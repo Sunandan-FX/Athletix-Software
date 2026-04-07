7bd7fa05a15cf7be97d31ad44fd45d57cf4f79a2
Sunandan Das <sunandandas131@gmail.com>
2026-04-07 22:54:41 +0600

    UserAdmin class in admin.py class
    Added a comment to indicate purpose of UserAdmin class.

--- Athletix/user/admin.py
+++ Athletix/user/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User, AthleteProfile, CoachProfile, MedicalProfile
 
-
+#user admin created with necessary equipment 
 class UserAdmin(BaseUserAdmin):
     list_display = ('email', 'name', 'role', 'is_active', 'is_staff', 'date_joined')
     list_filter = ('role', 'is_active', 'is_staff')
